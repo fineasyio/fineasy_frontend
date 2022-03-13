e8ee1cd41313059f824ee35b4622a61340735a89
Pedro Roberto <roberto19santana@gmail.com>
2022-03-12 22:46:40 -0300

    criando tela login

---
+++ lib/view/v_login.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class Login {
+  Widget paginaInicial(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Sample Code'),
+      ),
+    );
+  }
+}
