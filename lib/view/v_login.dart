4bda15593d76df7db0cf23a4d57ac23a563c8276
Pedro Roberto <roberto19santana@gmail.com>
2022-03-13 18:37:36 -0300

    criação dos botões entrar e cadatre-se

--- lib/view/v_login.dart
+++ lib/view/v_login.dart
@@ -1,11 +1,49 @@
+// ignore_for_file: deprecated_member_use, file_names
+
 import 'package:flutter/material.dart';
 
 class Login {
   Widget paginaInicial(BuildContext context) {
+    Alignment.bottomCenter;
+
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Sample Code'),
+      body: ListView(
+        children: [
+          botaoEntrar(context),
+          botaoCadastrar(context),
+        ],
       ),
     );
+    //);
+  }
+
+  Widget botaoEntrar(BuildContext context) {
+    return Container(
+        padding: const EdgeInsets.all(20),
+        margin: const EdgeInsets.fromLTRB(10, 400, 10, 10),
+        color: Colors.amber,
+        alignment: Alignment.center,
+        child: RaisedButton(
+          onPressed: () {},
+          child: const Text("Entrar"),
+          color: Colors.blue,
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+        ));
+  }
+
+  Widget botaoCadastrar(BuildContext context) {
+    return Container(
+        padding: const EdgeInsets.all(20),
+        margin: const EdgeInsets.fromLTRB(10, 10, 10, 10),
+        color: Colors.amber,
+        alignment: Alignment.center,
+        child: RaisedButton(
+          onPressed: () {},
+          child: const Text("Cadastra-se"),
+          color: Colors.blue,
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+        ));
   }
 }
